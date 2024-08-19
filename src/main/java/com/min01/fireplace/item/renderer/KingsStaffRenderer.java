b85818c3fa244eef30ec2107b06cbfe70fcf6f86
min <goyoumin2@gmail.com>
2024-08-20 00:34:58 +0900

    tweaks

--- src/main/java/com/min01/fireplace/item/renderer/KingsStaffRenderer.java
+++ src/main/java/com/min01/fireplace/item/renderer/KingsStaffRenderer.java
@@ -9,6 +9,7 @@ import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
@@ -21,11 +22,11 @@ import net.minecraft.world.item.Items;
 
 public class KingsStaffRenderer extends BlockEntityWithoutLevelRenderer
 {
-	public EntityModelSet set;
-	public KingsStaffRenderer(BlockEntityRenderDispatcher p_172550_, EntityModelSet p_172551_) 
+	public EntityModelSet modelSet;
+	public KingsStaffRenderer(BlockEntityRenderDispatcher p_172550_, EntityModelSet modelSet) 
 	{
-		super(p_172550_, p_172551_);
-		this.set = p_172551_;
+		super(p_172550_, modelSet);
+		this.modelSet = modelSet;
 	}
 	
 	@Override
@@ -34,16 +35,16 @@ public class KingsStaffRenderer extends BlockEntityWithoutLevelRenderer
 		p_108832_.pushPose();
 		p_108832_.scale(0.5F, 0.5F, 0.5F);
 		p_108832_.translate(1, 2.5, 1);
-		Minecraft.getInstance().getItemRenderer().renderStatic(new ItemStack(Items.COMMAND_BLOCK), ItemTransforms.TransformType.NONE, 240, OverlayTexture.NO_OVERLAY, p_108832_, p_108833_, 0);
+		Minecraft.getInstance().getItemRenderer().renderStatic(new ItemStack(Items.COMMAND_BLOCK), ItemTransforms.TransformType.NONE, LightTexture.FULL_BLOCK, OverlayTexture.NO_OVERLAY, p_108832_, p_108833_, 0);
 		p_108832_.popPose();
 		
 		p_108832_.pushPose();
-		p_108832_.mulPose(Vector3f.YP.rotationDegrees(180F));
-		p_108832_.mulPose(Vector3f.XP.rotationDegrees(180F));
-		p_108832_.translate(0, -1.5, 0);
-		ModelKingsStaff model = new ModelKingsStaff(this.set.bakeLayer(ModelKingsStaff.LAYER_LOCATION));
-		VertexConsumer consumer = p_108833_.getBuffer(RenderType.entitySolid(new ResourceLocation(Fireplace.MODID, "textures/items/king_staff.png")));
-		model.renderToBuffer(p_108832_, consumer, 240, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+		p_108832_.mulPose(Vector3f.YP.rotationDegrees(180.0F));
+		p_108832_.mulPose(Vector3f.XP.rotationDegrees(180.0F));
+		p_108832_.translate(0, -1.5F, 0);
+		ModelKingsStaff model = new ModelKingsStaff(this.modelSet.bakeLayer(ModelKingsStaff.LAYER_LOCATION));
+		VertexConsumer consumer = p_108833_.getBuffer(RenderType.entityCutoutNoCull(new ResourceLocation(Fireplace.MODID, "textures/items/king_staff.png")));
+		model.renderToBuffer(p_108832_, consumer, LightTexture.FULL_BLOCK, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
 		p_108832_.popPose();
 	}
 }
